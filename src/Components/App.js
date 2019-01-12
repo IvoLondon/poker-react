1b4d1c91ecd0c11ad129d7cb22615a061cde9265
Ivo <ivelin.iliev@hotmail.com>
2019-01-12 20:03:04 +0000

    Add, Remove, Deal Cards to two players

--- src/Components/App.js
+++ src/Components/App.js
@@ -7,7 +7,189 @@ import Deck from "./Deck";
 import { Card, PlayerHand, Button, Footer } from "../Styles/Styled";
 
 class App extends Component {
+	constructor() {
+		super();
+		this.cardsHolder = [];
+		this.state = {
+			players : [
+				{
+					id: 0,
+					name  : 'Player 1 name',
+					editMode : false,
+					cardsInHand : [],
+				},
+				{
+					id: 1,
+					name  : 'Player 2 name',
+					editMode : false,
+					cardsInHand : [],
+				}
+			],
+			cardsInGame : [],
+		}
+	}
+
+	componentDidMount() {
+		this.dealCardsHandler();
+	}
+
+	dealCardsHandler = () => {
+		const newState = { ...this.state }
+		const playersList = [ ...newState.players ]
+
+		playersList.map((player, idx) => {
+		const cardsList = [...playersList[idx].cardsInHand]; 
+
+			for(let i = 0; i < 5; i++) {
+				cardsList.push(this.createCard());
+			}
+			playersList[idx].cardsInHand = cardsList;
+		})
+
+		console.log(playersList);
+		this.setState({
+			players : playersList
+		})
+	}
+	createCard() {
+		let cardValue = values[Math.random() * values.length | 0];
+		let cardSuit = suits[Math.random() * suits.length | 0];
+		let card = cardValue+cardSuit;
+
+		let checkCard = this.cardsHolder.find((cardIn) => { 
+			return cardIn === card;
+		});
+
+		if(checkCard === undefined) {
+
+			this.cardsHolder.push(card);
+			this.setState(prevState => ({
+				cardsInGame : [ ...prevState.cardsInGame, card]
+			}))
+			return {
+				value : cardValue,
+				suit : cardSuit
+			}
+			
+		} else {
+			console.log('duplicate, create new', card)
+			return this.createCard();
+		}		
+	}
+
+	addPlayerHandler = () => {
+		const newState = { ...this.state }
+		if(newState.players.length >= 6) {
+			alert('Maximum number of players is reached');
+			return
+		}
+		const playersList = [...newState.players];
+
+		let newPlayer = {
+			id: newState.players.length,
+			name : 'Player Name',
+			editMode : false,
+			cardsInHand : [],
+		}
+		for (var i=0; i < playersList.length; i++) {
+	        if (playersList[i].id === newPlayer.id) {
+	        	newPlayer.id++;
+	        }
+	    }
+	    
+		this.setState({
+			players : newState.players.concat([newPlayer]),
+		})
+		this.dealCardsHandler();
+	}
+	removePlayerHandler = (id) => {
+		const player = {...id}
+		const newState = { ...this.state }
+		if(newState.players.length <= 2) {
+			alert('Maximum number of 2 players is reached')
+			return
+		}
+		const playersList = newState.players.concat();
+		for (var i=0; i < playersList.length; i++) {
+	        if (playersList[i].id === player.id) {
+	        	playersList.splice(i, 1);
+	        }
+	    }
+	    this.setState({
+	    	players : playersList
+	    })
+	}
+	editPlayerHandler = (id) => {
+		const player = { ...id }
+		const newState = { ...this.state }
+		const playersList = [...newState.players];
+		for (var i=0; i < playersList.length; i++) {
+	        if (playersList[i].id === player.id) {
+	        	playersList[i].editMode = !player.editMode;
+	        }
+	    }
+	    this.setState({
+			players : playersList,
+		})
+	}
+	enterPlayerNameHandler = (event, id) => {
+		const player = { ...id }
+		player.name = event.target.value;
+		const newState = { ...this.state }
+		const playersList = [...newState.players];
+
+
+		for (var i=0; i < playersList.length; i++) {
+	        if (playersList[i].id === player.id) {
+	        	playersList[i].name = event.target.value;
+	        }
+	    }
+	    this.setState({
+			players : playersList,
+		})
+	}
+
+
+
+	checkWinnerHandler = () => {
+		
+		
+	}
+
 	render() {
+		let players = null;
+		if(this.state.players) {
+			players = this.state.players.map(player => {
+				return (
+				
+				<article key={player.id}>
+					<p>
+						{player.editMode ? <input value={player.name} onChange={(event) => this.enterPlayerNameHandler.call(this, event, player)} /> : player.name}
+						
+						<Button onClick={() => this.editPlayerHandler.call(this, player)}>
+							<span role="img" alt="pencil" aria-label="pencil">✏️</span>
+							{player.editMode ? 'Confirm' : 'Edit'}
+						</Button>
+						<Button onClick={() => this.removePlayerHandler.call(this, player)}>
+							<span role="img" alt="flame" aria-label="flame">🔥</span>
+							Remove
+						</Button>
+					</p>
+					<PlayerHand>
+						{player.cardsInHand.map((card) => {
+							return (
+								<Card key={card.suit+card.value} suit={card.suit} value={card.value} selected={true}>
+									{card.value}
+								</Card>
+							)
+						})}
+					</PlayerHand>
+				</article>
+				)
+				
+			})
+		}
+
 		return (
 				<Layout>
 
@@ -21,74 +203,20 @@ class App extends Component {
 						<header>
 							<h1>Players</h1>
 						</header>
+
+
 						<section>
-							<article>
-								<p>
-									Player 1 name
-									<Button>
-										<span role="img" alt="pencil" aria-label="pencil">✏️</span>
-										Edit
-									</Button>
-									<Button>
-										<span role="img" alt="flame" aria-label="flame">🔥</span>
-										Remove
-									</Button>
-								</p>
-								<PlayerHand>
-										<Card suit="D" value="A" selected={true}>
-											A
-										</Card>
-										<Card suit="D" value="K">
-											K
-										</Card>
-										<Card suit="D" value="Q">
-											Q
-										</Card>
-										<Card suit="D" value="J">
-											J
-										</Card>
-										<Card suit="D" value="T">
-											T
-										</Card>
-								</PlayerHand>
-							</article>
-							<article>
-								<p>
-									Player 2 name
-									<Button>
-										<span role="img" alt="pencil" aria-label="pencil">✏️</span>
-										Edit
-									</Button>
-									<Button>
-										<span role="img" alt="flame" aria-label="flame">🔥</span>
-										Remove
-									</Button>
-								</p>
-								<PlayerHand>
-										<Card suit="S" value="A">
-											A
-										</Card>
-										<Card suit="S" value="K">
-											K
-										</Card>
-										<Card suit="S" value="Q" selected={true}>
-											Q
-										</Card>
-										<Card suit="S" value="J">
-											J
-										</Card>
-										<Card suit="S" value="T">
-											T
-										</Card>
-								</PlayerHand>
-							</article>
+							
+							{players}
+
+							
 						</section>
 						<Footer>
-								<Button>
+								<Button onClick={() => this.addPlayerHandler()}>
 									<span role="img" alt="woman raising hand" aria-label="woman raising hand">🙋‍♀️</span>
 									Add new player
 								</Button>
-								<Button>
+								<Button onClick={() => this.dealCardsHandler()}>
 									<span role="img" alt="trophy" aria-label="trophy">🏆</span>
 									Find the winner
 								</Button>
